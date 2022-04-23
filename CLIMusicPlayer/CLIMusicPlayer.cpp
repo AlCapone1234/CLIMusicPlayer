856abfdb966a944bd8308260693dd1820c248a27
Emilio Ganibegovic <alcaponereal@outlook.com>
2022-04-23 19:28:49 +0200

    Added test for music, automatically create the music directory if it hasn't been created.

--- CLIMusicPlayer/CLIMusicPlayer.cpp
+++ CLIMusicPlayer/CLIMusicPlayer.cpp
@@ -1,20 +1,32 @@
-// CLIMusicPlayer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <filesystem>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Check if music directory exits and create it
+    if (std::filesystem::is_directory("music") || !std::filesystem::exists("music"))
+    {
+        std::filesystem::create_directory("music");
+    }
+
+   
+    // get the path for music folder
+    std::wstring path = std::filesystem::current_path().wstring() + L"/music";
+    
+
+    // Change it to correct the /
+    for (int i = 0; i < path.length(); i++)
+    {
+        if (path[i] == '\\')
+            path[i] = '/';
+    }
+
+
+    bool played = PlaySound(path.c_str(), NULL, SND_SYNC);
+    std::cout << played;
+
+    std::cin.get();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
